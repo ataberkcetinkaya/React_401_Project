5ec7ab0df13044bcfd7ead6a36c3722c5ce18887
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2023-04-17 01:00:36 +0300

    Register steps are done with backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,3 +9,8 @@ export const getProductById = async (product_id) => {
     const { data } = await axios.get('http://localhost:4000/product/' + product_id)
     return data
 }
+
+export const fetchRegister = async (values) => {
+    const { data } = await axios.post('http://localhost:4000/auth/register', values)
+    return data
+}
--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Flex, Box, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react'
+import { Flex, Box, Heading, FormControl, FormLabel, Input, Button, Alert } from '@chakra-ui/react'
 import { useFormik } from 'formik'
 import validationSchema from './validations'
+import { fetchRegister } from '../../api'
 
 export default function Register() {
 
@@ -13,7 +14,12 @@ export default function Register() {
     },
     validationSchema,
     onSubmit: async (values,bag) => {
-      console.log(values);
+      try {
+        const response = await fetchRegister({ email: values.email, password: values.password })
+      }
+      catch (error) {
+        bag.setErrors({ general: error.response.data.message })
+      }
     }
   })
 
@@ -28,22 +34,30 @@ export default function Register() {
             <form onSubmit={formik.handleSubmit}>
               <FormControl>
                 <FormLabel>E-mail</FormLabel>
-                <Input name="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email}></Input>
+                <Input name="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} isInvalid={formik.touched.email && formik.errors.email}></Input>
               </FormControl>
 
               <FormControl mt="4">
                 <FormLabel>Password</FormLabel>
-                <Input name="password" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password}></Input>
+                <Input name="password" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} isInvalid={formik.touched.password && formik.errors.password}></Input>
               </FormControl>
 
               <FormControl mt="4">
                 <FormLabel>Confirm Password</FormLabel>
-                <Input name="confirmPassword" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.confirmPassword}></Input>
+                <Input name="confirmPassword" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.confirmPassword} isInvalid={formik.touched.confirmPassword && formik.errors.confirmPassword}></Input>
               </FormControl>
 
               <Button mt="4" width="50%" type="submit" colorScheme='red'>
                 Register
               </Button>
+
+              <Box mt="4">
+                {formik.errors.general && (
+                  <Alert status="error" colorScheme='red'>
+                    {formik.errors.general}
+                  </Alert>
+                )}
+              </Box>
             </form>
           </Box>
         </Box>
